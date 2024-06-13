b8e539b41bb581e1d8f2f696d4cd4a5d5ba7deb1
Robert Hlobik <roberthlobik09@gmail.com>
2024-06-13 14:36:18 +0200

    Zadaca MySQL - stvaranje baze i tablica

--- tvrtka_create.sql
+++ tvrtka_create.sql
@@ -1,13 +1,37 @@
-DROP DATABASE IF EXISTS tvrtka
+DROP DATABASE IF EXISTS tvrtka;
 
 CREATE DATABASE IF NOT EXISTS tvrtka DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE tvrtka;
 
+CREATE TABLE IF NOT EXISTS grad (
+    id INT UNSIGNED AUTO_INCREMENT  NOT NULL PRIMARY KEY,
+    ime VARCHAR(50) NOT NULL,
+    zip VARCHAR(5) NOT NULL
+)ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS poslovnice(
     id INT UNSIGNED AUTO_INCREMENT  NOT NULL PRIMARY KEY,
     ime VARCHAR(20) NOT NULL,
-    adresa VARCHAR(255),
-    broj_telefona VARCHAR(12),
-    grad VARCHAR(20)
+    adresa VARCHAR(255) NOT NULL,
+    broj_telefona VARCHAR(12)
 )ENGINE=InnoDB;
+
+ALTER TABLE poslovnice DROP COLUMN broj_telefona;
+ALTER TABLE poslovnice ADD COLUMN voditelj_id INT UNSIGNED NOT NULL AFTER adresa;
+ALTER TABLE poslovnice ADD COLUMN grad_id INT UNSIGNED NOT NULL;
+
+
+
+CREATE TABLE IF NOT EXISTS zaposlenici (
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    ime VARCHAR(50) NOT NULL,
+    adresa VARCHAR(255) NOT NULL,
+    poslovnica_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (poslovnica_id) REFERENCES poslovnice(id),
+    grad_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (grad_id) REFERENCES grad(id)
+)ENGINE=InnoDB;
+
+ALTER TABLE poslovnice ADD FOREIGN KEY (voditelj_id) REFERENCES zaposlenici(id);
+ALTER TABLE poslovnice ADD FOREIGN KEY (grad_id) REFERENCES grad(id);

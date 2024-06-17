a36e0de12da7c7f96abf1d43b88cf66eb9e722b1
Robert Hlobik <roberthlobik09@gmail.com>
2024-06-17 18:03:43 +0200

    Zadaca2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Osnove MySQL
 #### Zadaca MySQL - stvaranje baze i tablica 
 
-- Primjer SQL koda zadace mozete pronaći na [ovoj](https://github.com/robitest/osnove_mysql/blob/master/videoteka.sql) poveznici
+- Primjer SQL koda zadace mozete pronaći na [ovoj](https://github.com/robitest/osnove_mysql/blob/master/videoteka.sql) poveznici.
---
+++ videoteka_insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO zanrovi (ime)
+VALUES
+('Action'),
+('Comedy'),
+('Drama');
+
+INSERT INTO filmovi (naslov, godina, zanr_id)
+VALUES
+('Inception', 1, 1),
+('The Hangover', 2, 2),
+('The Godfather', 3, 1); 
+
+INSERT INTO cjenik (tip_filma, cijena, zakasnina_po_danu)
+VALUES
+('Jit', 3.50, 1.50),
+('Regular', 2.00, 1.00),
+('Old', 1.00, 0.50); 
+
+INSERT INTO mediji (tip, koeficijent)
+VALUES
+('Kazeta', 1),
+('DVDr', 1.2),
+('BlueRay', 1.50); 
+
+INSERT INTO clanovi (ime, adresa, telefon, email, clanski_broj)
+VALUES
+('Jhon Doe', '123 Eln Street', '555-1234', 'a@b.com', 'V001'),
+('Jane Smith', '456 Oak Avenue', '555-5678', 'a@b2.com', 'V002'),
+('Alice Johnson', '789 Pine Road', '555-9012', 'a@b3.com', 'V003'),
